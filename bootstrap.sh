345fa3b5ed532462762e14bd7d6f9883d60c5449
Robby <robby.cowell@gmail.com>
2019-03-29 14:03:15 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-curl --silent --location https://deb.nodesource.com/setup_8.x | sudo bash -
+curl --silent --location https://deb.nodesource.com/setup_11.x | sudo bash -
 
 sudo apt-get update
 
 sudo apt-get -y install git nodejs
+
+sudo chown -R vagrant:vagrant /usr/lib/node_modules
+sudo chown -R vagrant:vagrant /usr/bin/
+
+npm install strapi@alpha -g
